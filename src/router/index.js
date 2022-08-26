883d5fd2b2449c8c53179952c70b4e31f6272904
TanPham275 <88332691+TanPham275@users.noreply.github.com>
2022-08-26 14:37:09 +0700

    Create index.js

---
+++ src/router/index.js
@@ -0,0 +1,37 @@
+import { createRouter, createWebHashHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import HomeView2 from "../views/HomeView2.vue";
+
+
+
+const routes = [
+  {
+    path: "/",
+    name: "home",
+    component: HomeView,
+  },
+  {
+    path: "/about",
+    name: "about",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+  },
+  {
+    path: "/tan-pham",
+    name: "tanpham",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component:HomeView2,
+  }
+];
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+});
+
+export default router;
