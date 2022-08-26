bb16ddd7c2de041177a2254fdbaba8131afee955
TanPham275 <88332691+TanPham275@users.noreply.github.com>
2022-08-26 14:36:59 +0700

    Create HomeView2.vue

---
+++ src/views/HomeView2.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld2 msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld2 from "@/components/HelloWorld2.vue";
+
+export default {
+  name: "HomeView2",
+  components: {
+    HelloWorld2,
+  },
+};
+</script>
